0a735b74bc11ca8ee34b0933697f5eaeba0dd2d8
wladyDE <vovawestland@gmail.com>
2025-01-27 19:35:35 +0100

    add static carousel component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { Carousel } from './layouts/HomePage/Carousel'
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks'
 import { Navbar } from './layouts/Navbar/Navbar'
 
 function App() {
   return (
-    <Navbar/>
+    <>
+        <Navbar/>
+        <ExploreTopBooks/>
+        <Carousel/>
+    </>
   )
 }
 
