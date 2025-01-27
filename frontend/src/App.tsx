3067787c815f0a611f7020d8bf92679a9a887357
wladyDE <vovawestland@gmail.com>
2025-01-27 19:44:25 +0100

    add Heroes and LibraryServices components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Carousel } from './layouts/HomePage/Carousel'
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks'
+import { Heros } from './layouts/HomePage/Heroes'
+import { LibraryServices } from './layouts/HomePage/LibraryServices'
 import { Navbar } from './layouts/Navbar/Navbar'
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
         <Navbar/>
         <ExploreTopBooks/>
         <Carousel/>
+        <Heros/>
+        <LibraryServices/>
     </>
   )
 }
